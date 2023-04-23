c404c88f4bd7f289541b89a7c41bddc91c47f5d8
GhosTHaise <Ghostrex2@gmail.com>
2023-04-23 21:26:01 +0300

    Init tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        satoshi : ["Satoshi","sans-serif"],
+        inter : ["inter","sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
