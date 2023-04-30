6cf5894386eaf00342d2a327be56b25c3f40f826
GhosTHaise <Ghostrex2@gmail.com>
2023-04-30 18:14:54 +0300

    get api key from rapidapi and build base query

--- src/services/article.js
+++ src/services/article.js
@@ -4,11 +4,20 @@ import  {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 export const articleApi = createApi({
     reducerPath : "articleApi",
     baseQuery : fetchBaseQuery({
-        baseUrl : ""
-    }),
+        baseUrl : "https://article-extractor-and-summarizer.p.rapidapi.com/",
+        prepareHeaders : (headers) => {
+            headers.set("content-type",'application/json');
+            headers.set("X-RapidAPI-Key", import.meta.env.VITE_RAPID_API_ARTICLE_KEY);
+            headers.set("X-RapidAPI-Host",import.meta.env.VITE_RAPID_API_ARTICLE_HOST);
+        
+            return headers;
+        }
+    
+    }
+    ),
     endpoints: (builder) => ({
         getSummary : builder.query({
-            query : (params) => "test"
+            query : (params) => `/summarize?url=${params.article.url}&length=3`
         })
     })
 })
