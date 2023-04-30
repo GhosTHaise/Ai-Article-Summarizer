32f876083619714173f8879bb3e69fe727cf26c1
GhosTHaise <Ghostrex2@gmail.com>
2023-04-30 17:30:19 +0300

    create state to my form and inport react-redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,13 +3,24 @@ import { useState,useEffect } from "react";
 import {copy ,linkIcon ,loader ,tick} from "../assets"
 
 const Demo = () => {
+
+  const [article, setArticle] = useState({
+    url : '',
+    summary : ''
+  });
+
+  const handleSubmit = async(e) => {
+      //e.prevent.default();
+      alert("submitted")
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
         {/* Search */}
         <div className="flex flex-col w-full gap-2">
             <form 
             className="relative flex justify-center items-center"
-            onSubmit={()=>{}}
+            onSubmit={handleSubmit}
             >
                 <img 
                 src={linkIcon}
@@ -19,19 +30,26 @@ const Demo = () => {
                 <input 
                 type="url" 
                 placeholder="Enter a URL"
-                value={""}
-                onChange={()=>{}}
+                value={article.url}
+                onChange={ 
+                  (e) => setArticle({...article,url : e.target.value }) 
+                }
                 required
                 className="url_input peer"
                 />
                 <button
                 type="submit"
-                className="submit_btn"
+                className="submit_btn 
+                peer-focus:border-gray-700 
+                peer-focus:text-gray-700"
                 >
                     ↲
                 </button>
             </form>
+            {/* Browse URL History */}
         </div>
+
+        {/* Display Results*/}
     </section>
   )
 }
